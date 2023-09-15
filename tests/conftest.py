1ef9ed9b02091e9a1a4ef5f9a738f07a215f599a
Logi E. <logi.eyjolfsson@gmail.com>
2023-09-15 11:47:14 +0000

    Clean up pyproject and conftest.

--- tests/conftest.py
+++ tests/conftest.py
@@ -29,29 +29,34 @@ def anyio_backend():
 
 
 # The following is for marking specific tests
-# with `network` or `slow`.
+# with custom marks registered in pyproject.toml.
 #
-# These tests are *skipped by default*.
+# These marked tests are *skipped by default*.
 #
-# To mark a test decorate with:
+# To mark a test decorate it with e.g.
 # `@pytest.mark.network` or `@pytest.mark.slow`
-# Run the tests with the flags:
-# `--run-network`, `--run-slow`, `--run-all`
+#
+# Run the marked tests, use the corresponding
+# CLI options e.g. `--run-network` or `--run-slow`
+
+SKIP_MARKERS = (
+    "network",
+    "slow",
+)
 
 
 def pytest_addoption(parser: pytest.Parser):
-    parser.addoption(
-        "--run-network",
-        action="store_true",
-        default=False,
-        help="run tests which communicate with external services",
-    )
-    parser.addoption(
-        "--run-slow",
-        action="store_true",
-        default=False,
-        help="run slow tests",
-    )
+    """Add options to the pytest CLI interface."""
+    for marker in SKIP_MARKERS:
+        # Add --run-{marker} arguments for each skip marker
+        parser.addoption(
+            f"--run-{marker}",
+            action="store_true",
+            default=False,
+            help=f"run {marker} tests",
+        )
+
+    # The option `--run-all` is special, it runs *all* tests
     parser.addoption(
         "--run-all",
         action="store_true",
@@ -60,39 +65,24 @@ def pytest_addoption(parser: pytest.Parser):
     )
 
 
-def pytest_configure(config: pytest.Config):
-    config.addinivalue_line(
-        "markers", "network: mark test as communicating with external services"
-    )
-    config.addinivalue_line("markers", "slow: mark test as slow")
-
-
 def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item]):
+    """Add skip markers to tests, based on how pytest was invoked."""
     if config.getoption("--run-all"):
         # --run-all flag provided: don't skip any tests
         return
 
-    # Map marker names to skip markers,
-    # indicating the correct flag to use to run the tests.
-    skip_markers = {
-        "network": pytest.mark.skip(reason="run with '--run-network'"),
-        "slow": pytest.mark.skip(reason="run with '--run-slow'"),
+    # Set of markers which weren't specified to run with `--run-{marker}`
+    skipped: set[str] = {
+        marker for marker in SKIP_MARKERS if not config.getoption(f"--run-{marker}")
     }
 
-    if config.getoption("--run-network"):
-        # --run-network given in cli: do not skip network tests
-        del skip_markers["network"]
-    if config.getoption("--run-slow"):
-        # --run-slow given in cli: do not skip slow tests
-        del skip_markers["slow"]
-
-    if len(skip_markers) == 0:
-        # All flags given independently
-        # (if someone likes typing long commands?)
+    if len(skipped) == 0:
+        # All --run-... options given independently
+        # (someone likes typing long commands?)
         return
 
-    # Mark relevant tests as being skipped
+    # For each test item, find whether it has a mark which should be skipped
     for item in items:
-        for kw, skip_mark in skip_markers.items():
-            if kw in item.keywords:
-                item.add_marker(skip_mark)
+        for marker in skipped.intersection(m.name for m in item.own_markers):
+            # Add `pytest.mark.skip` to this test item
+            item.add_marker(pytest.mark.skip(reason=f"run with --run-{marker}"))
